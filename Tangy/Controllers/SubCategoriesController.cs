ac4306f98c59ba9797fba157e053efd7e0c08324
Ben Spark <bhrugen1990@gmail.com>
2018-03-31 13:36:54 -0500

    Section 5 - SubCategory Index Action Method

---
+++ Tangy/Controllers/SubCategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+
+namespace Tangy.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET Action 
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = _db.SubCategory.Include(s => s.Category);
+
+            return View(await subCategories.ToListAsync());
+        }
+    }
+}
