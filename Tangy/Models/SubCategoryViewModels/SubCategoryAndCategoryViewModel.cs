e3cc05a953c719ffeaf81547a2e7d40943e62aa2
Ben Spark <bhrugen1990@gmail.com>
2018-03-31 13:29:50 -0500

    Section 5 - Category and subcategory view model

---
+++ Tangy/Models/SubCategoryViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models.SubCategoryViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public SubCategory SubCategory { get; set; }
+        public IEnumerable<Category> CategoryList { get; set; }
+
+        public List<string> SubCategoryList { get; set; }
+
+        [Display(Name ="New Sub Category")]
+        public bool isNew { get; set; }
+
+        public string StatusMessage { get; set; }
+
+    }
+}
