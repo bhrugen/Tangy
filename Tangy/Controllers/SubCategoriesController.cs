6d36dbdad3b7c9a952864ad48982394aeb018bf7
Ben Spark <bhrugen1990@gmail.com>
2018-03-31 13:54:58 -0500

    Section 5 - SubCategory Create Get Action Method

--- Tangy/Controllers/SubCategoriesController.cs
+++ Tangy/Controllers/SubCategoriesController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Tangy.Data;
+using Tangy.Models;
+using Tangy.Models.SubCategoryViewModels;
 
 namespace Tangy.Controllers
 {
@@ -24,5 +26,19 @@ namespace Tangy.Controllers
 
             return View(await subCategories.ToListAsync());
         }
+
+        //GET Action for Create
+        public IActionResult Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToList()
+            };
+
+            return View(model);
+        }
+
     }
 }
