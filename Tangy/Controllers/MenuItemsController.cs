19587f0ff77dabf4784428cf4122900771dff3e4
Ben Spark <bhrugen1990@gmail.com>
2018-04-01 01:21:41 -0500

    Section 6 - Menu Item Create Post Action Method

--- Tangy/Controllers/MenuItemsController.cs
+++ Tangy/Controllers/MenuItemsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Tangy.Data;
 using Tangy.Models;
 using Tangy.Models.MenuItemViewModels;
+using Tangy.Utility;
 
 namespace Tangy.Controllers
 {
@@ -45,6 +47,53 @@ namespace Tangy.Controllers
         {
             return View(MenuItemVM);
         }
+
+        //POST : MenuItems Create
+        [HttpPost,ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePOST()
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+            if(!ModelState.IsValid)
+            {
+                return View(MenuItemVM);
+            }
+
+            _db.MenuItem.Add(MenuItemVM.MenuItem);
+            await _db.SaveChangesAsync();
+
+            //Image Being Saved
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var menuItemFromDb = _db.MenuItem.Find(MenuItemVM.MenuItem.Id);
+
+            if(files[0]!=null && files[0].Length>0)
+            {
+                //when user uploads an image
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = files[0].FileName.Substring(files[0].FileName.LastIndexOf("."), files[0].FileName.Length - files[0].FileName.LastIndexOf("."));
+
+                using (var filestream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+                menuItemFromDb.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension;
+            }
+            else
+            {
+                //when user does not upload image
+                var uploads = Path.Combine(webRootPath, @"images\"+ SD.DefaultFoodImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\images\" + MenuItemVM.MenuItem.Id + ".png");
+                menuItemFromDb.Image= @"\images\" + MenuItemVM.MenuItem.Id + ".png";
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
 
         public JsonResult GetSubCategory(int CategoryId)
         {
---
+++ Tangy/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Utility
+{
+    public class SD
+    {
+        public const string DefaultFoodImage = "default_food.png";
+    }
+}
