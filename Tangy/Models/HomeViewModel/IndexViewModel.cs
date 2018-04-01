6b438ab3e812a5a2b4dfe938d555a967e09f20c2
Ben Spark <bhrugen1990@gmail.com>
2018-04-01 13:06:22 -0500

    Section 8 - Index View Model

---
+++ Tangy/Models/HomeViewModel/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models.HomeViewModel
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<MenuItem> MenuItem { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<Coupons> Coupons { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
