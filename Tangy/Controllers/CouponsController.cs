4822c27d71c7447cf496243ea5aab549fb5d3a10
Ben Spark <bhrugen1990@gmail.com>
2018-04-01 11:38:10 -0500

    Section 7 - Coupons Controller and Index Action Method

---
+++ Tangy/Controllers/CouponsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+
+namespace Tangy.Controllers
+{
+    public class CouponsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupons.ToListAsync());
+        }
+    }
+}
