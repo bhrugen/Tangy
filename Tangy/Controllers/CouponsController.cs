65f0296c47b2312c05df04f35acd79042c5e83c3
Ben Spark <bhrugen1990@gmail.com>
2018-04-01 11:50:25 -0500

    Section 7 - Coupons Create Get and Post Action Method

--- Tangy/Controllers/CouponsController.cs
+++ Tangy/Controllers/CouponsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Tangy.Data;
+using Tangy.Models;
 
 namespace Tangy.Controllers
 {
@@ -21,5 +23,42 @@ namespace Tangy.Controllers
         {
             return View(await _db.Coupons.ToListAsync());
         }
+
+
+        //GET Coupons
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create Coupons
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupons coupons)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if(files[0]!=null && files[0].Length>0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                    _db.Coupons.Add(coupons);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(coupons);
+        }
+
+
     }
 }
