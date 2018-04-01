24b71b5252ec9a7564eda3a8f312190ba2c0ce81
Ben Spark <bhrugen1990@gmail.com>
2018-04-01 13:11:40 -0500

    Section 8 - Home Controller Index Action Method

--- Tangy/Controllers/HomeController.cs
+++ Tangy/Controllers/HomeController.cs
@@ -4,31 +4,34 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
 using Tangy.Models;
+using Tangy.Models.HomeViewModel;
 
 namespace Tangy.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> Index()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Category = _db.Category.OrderBy(c=>c.DisplayOrder),
+                Coupons = _db.Coupons.Where(c=>c.isActive==true).ToList()
+            };
+            return View(IndexVM);
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
