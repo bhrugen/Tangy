16f23ecfc0f0233a679aef8648f315d147c39985
Ben Spark <bhrugen1990@gmail.com>
2018-04-03 20:29:23 -0500

    Section 9 - Adding fileds to Manage Account

--- Tangy/Models/ManageViewModels/IndexViewModel.cs
+++ Tangy/Models/ManageViewModels/IndexViewModel.cs
@@ -21,5 +21,11 @@ namespace Tangy.Models.ManageViewModels
         public string PhoneNumber { get; set; }
 
         public string StatusMessage { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
     }
 }
