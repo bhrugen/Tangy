3267af233e0bbb28f8a8dfecfda69a71797a6321
Ben Spark <bhrugen1990@gmail.com>
2018-04-06 15:51:00 -0500

    Section 13 - Adding Order Status Constants

--- Tangy/Utility/SD.cs
+++ Tangy/Utility/SD.cs
@@ -12,6 +12,12 @@ namespace Tangy.Utility
         public const string AdminEndUser = "Admin";
         public const string CustomerEndUser = "Customer";
 
+        public const string StatusSubmitted = "Submitted";
+        public const string StatusInProcess = "Being Prepared";
+        public const string StatusReady = "Ready for Pickup";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+
 
     }
 }
