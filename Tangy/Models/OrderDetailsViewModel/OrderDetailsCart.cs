ebe9cba64ea5897c76fae8c2c2f2f59dd923dbfd
Ben Spark <bhrugen1990@gmail.com>
2018-04-06 10:37:27 -0500

    Section 13 - Cart Details View Model

---
+++ Tangy/Models/OrderDetailsViewModel/OrderDetailsCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models.OrderDetailsViewModel
+{
+    public class OrderDetailsCart
+    {
+        public List<ShoppingCart> listCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
