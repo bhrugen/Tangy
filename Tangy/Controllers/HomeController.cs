5922a0e5414f995c180cbd6075a4381e0944e748
Ben Spark <bhrugen1990@gmail.com>
2018-04-05 23:24:31 -0500

    Section 12 - Home controller Details Post action Method

--- Tangy/Controllers/HomeController.cs
+++ Tangy/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Tangy.Data;
@@ -31,7 +33,7 @@ namespace Tangy.Controllers
             return View(IndexVM);
         }
 
-
+        [Authorize]
         public async Task<IActionResult> Details(int id)
         {
             var MenuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == id).FirstOrDefaultAsync();
@@ -45,10 +47,49 @@ namespace Tangy.Controllers
             return View(CartObj);
         }
 
-        
-        public IActionResult Error()
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(ShoppingCart CartObject)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            CartObject.Id = 0;
+            if(ModelState.IsValid)
+            {
+                var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                CartObject.ApplicationUserId = claim.Value;
+
+                ShoppingCart cartFromDb = await _db.ShoppingCart.Where(c => c.ApplicationUserId == CartObject.ApplicationUserId
+                                                    && c.MenuItemId == CartObject.MenuItemId).FirstOrDefaultAsync();
+                
+                if(cartFromDb==null)
+                {
+                    //this menu item does not exists
+                    _db.ShoppingCart.Add(CartObject);
+                }
+                else
+                {
+                    //menu item exists in shopping cart for that user, so just update the count
+                    cartFromDb.Count = cartFromDb.Count + CartObject.Count;
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var MenuItemFromDb = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).Where(m => m.Id == CartObject.MenuItemId).FirstOrDefaultAsync();
+
+                ShoppingCart CartObj = new ShoppingCart()
+                {
+                    MenuItem = MenuItemFromDb,
+                    MenuItemId = MenuItemFromDb.Id
+                };
+
+                return View(CartObj);
+            }
         }
+        
     }
 }
--- Tangy/Models/ShoppingCart.cs
+++ Tangy/Models/ShoppingCart.cs
@@ -21,7 +21,7 @@ namespace Tangy.Models
         [ForeignKey("ApplicationUserId")]
         public virtual ApplicationUser ApplicationUser { get; set; }
 
-        [Range(1,int.MaxValue,ErrorMessage ="Please enter a value greater than {1}")]
+        [Range(1,int.MaxValue,ErrorMessage ="Please enter a value greater than or equal to {1}")]
         public int Count { get; set; }
 
         [NotMapped]
