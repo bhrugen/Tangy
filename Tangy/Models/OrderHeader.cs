7de790c17e02643f45538887ac79adf8ef04e1b2
Ben Spark <bhrugen1990@gmail.com>
2018-04-06 10:22:26 -0500

    Section 13 - Add Order Header Model

---
+++ Tangy/Models/OrderHeader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models
+{
+    public class OrderHeader
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        public DateTime PickUpTime { get; set; }
+
+        public string CouponCode { get; set; }
+        public string Stauts { get; set; }
+        public string Comments { get; set; }
+
+    }
+}
