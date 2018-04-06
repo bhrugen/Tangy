221c7a203089804f015e6ae5e6804359309bc01f
Ben Spark <bhrugen1990@gmail.com>
2018-04-06 12:12:59 -0500

    Section 13 - Shopping Cart Get Action Method

---
+++ Tangy/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tangy.Data;
+using Tangy.Models;
+using Tangy.Models.OrderDetailsViewModel;
+
+namespace Tangy.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderDetailsCart detailCart { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            detailCart = new OrderDetailsCart()
+            {
+                OrderHeader = new OrderHeader()
+            };
+
+            detailCart.OrderHeader.OrderTotal = 0;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+            if(cart!=null)
+            {
+                detailCart.listCart = cart.ToList();
+            }
+
+            foreach(var list in detailCart.listCart)
+            {
+                list.MenuItem = _db.MenuItem.FirstOrDefault(m => m.Id == list.MenuItemId);
+                detailCart.OrderHeader.OrderTotal = detailCart.OrderHeader.OrderTotal + (list.MenuItem.Price * list.Count);
+
+                if(list.MenuItem.Description.Length>100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            detailCart.OrderHeader.PickUpTime = DateTime.Now;
+
+            return View(detailCart);
+        }
+    }
+}
