ffccca8d8bc1181bc6e6fc7ce76b0c9c9370ba2e
Ben Spark <bhrugen1990@gmail.com>
2018-04-06 21:26:45 -0500

    Section 14 - Create Order Details View Model

---
+++ Tangy/Models/OrderDetailsViewModel/OrderDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models.OrderDetailsViewModel
+{
+    public class OrderDetailsViewModel
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public List<OrderDetails> OrderDetail { get; set; }
+    }
+}
