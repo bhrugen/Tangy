0b3f564a62d0c6eceb3a2056dd048aa3e2b9cc0d
Ben Spark <bhrugen1990@gmail.com>
2018-04-07 14:35:01 -0500

    Section 15 - Paging Info Model

---
+++ Tangy/Models/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tangy.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int totalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsPerPage);
+    }
+}
