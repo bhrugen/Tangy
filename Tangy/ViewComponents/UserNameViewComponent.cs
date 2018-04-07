df68dcbe02bf68b390e9717dfbbad26a3b01a03c
Ben Spark <bhrugen1990@gmail.com>
2018-04-07 15:53:37 -0500

    Section 15 - Create View Componenet

---
+++ Tangy/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Tangy.Data;
+
+namespace Tangy.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.Users.Where(u => u.Id == claim.Value).FirstOrDefaultAsync();
+
+            return View(userFromDb);
+        }
+    }
+}
