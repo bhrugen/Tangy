6fbe9f0fbe8b8e4a6692960f2d87bc353f09a0ae
Ben Spark <bhrugen1990@gmail.com>
2018-04-07 17:24:19 -0500

    Section 15 - Sending Email EmailSender Class

--- Tangy/Services/EmailSender.cs
+++ Tangy/Services/EmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Tangy.Services
@@ -11,6 +13,22 @@ namespace Tangy.Services
     {
         public Task SendEmailAsync(string email, string subject, string message)
         {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("ben.spark92@gmail.com", "Temp1234*"),
+                EnableSsl = true
+            };
+
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress("ben.spark92@gmail.com")
+            };
+            mailMessage.To.Add(email);
+            mailMessage.Body = message;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Subject = subject;
+            client.Send(mailMessage);
             return Task.CompletedTask;
         }
     }
