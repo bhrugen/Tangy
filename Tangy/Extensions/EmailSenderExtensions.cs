6ed19ae84a39fbc0213d45b4a115e910167faac0
Ben Spark <bhrugen1990@gmail.com>
2018-04-07 17:33:09 -0500

    Section 15 - Send Email EmailSenderExtension

--- Tangy/Extensions/EmailSenderExtensions.cs
+++ Tangy/Extensions/EmailSenderExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Tangy.Services;
+using Tangy.Utility;
 
 namespace Tangy.Services
 {
@@ -13,6 +14,30 @@ namespace Tangy.Services
         {
             return emailSender.SendEmailAsync(email, "Confirm your email",
                 $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+        }
+
+        public static Task SendOrderStatusAsync(this IEmailSender emailSender, string email,string orderNumber, string status)
+        {
+            string subject = "";
+            string message = "";
+
+            if(status==SD.StatusCancelled)
+            {
+                subject = "Order Cancelled";
+                message = "Order Number " + orderNumber + " has been Cancelled! Please conatac us if you have any questions.";
+            }
+            if (status == SD.StatusSubmitted)
+            {
+                subject = "Order Created Successfully";
+                message = "Order Number " + orderNumber + " has been created sucessfully.";
+            }
+            if (status == SD.StatusReady)
+            {
+                subject = "Order Ready for Pickup";
+                message = "Order Number " + orderNumber + " is ready for Pickup! Please conatac us if you have any questions.";
+            }
+
+            return emailSender.SendEmailAsync(email, subject, message);
         }
     }
 }
