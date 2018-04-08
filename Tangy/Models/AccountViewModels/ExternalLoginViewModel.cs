36c448d43112f3476034a684dc8711aa793711ba
Ben Spark <bhrugen1990@gmail.com>
2018-04-08 00:44:28 -0500

    Section 15 - Adding Fields to External Login

--- Tangy/Models/AccountViewModels/ExternalLoginViewModel.cs
+++ Tangy/Models/AccountViewModels/ExternalLoginViewModel.cs
@@ -11,5 +11,17 @@ namespace Tangy.Models.AccountViewModels
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
     }
 }
